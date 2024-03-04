d679379cb2d54a627d5c2a377990bd953b1382b6
johyenho <93415119+johyenho@users.noreply.github.com>
2024-03-04 14:49:31 +0900

    4_package
    -packagejson 화일 생성

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-//require : calc.js화일을 연결하여 가져오는 명령어
+const randomColor = require('randomcolor');
+let color1 = randomColor();
+let color2 = randomColor();
+let color3 = randomColor();
 
-const calc = require("./calc.js");
-//require  요구하라는 자바스크립트 명령어
-console.log(("1 + 2 = "), calc.add(1, 2));
-console.log(("4 + 5 = "), calc.add(4, 5));
-console.log(("10 + 2 = "), calc.sub(10, 2));
-
-//모듈 시스템이란? module.exports로 내보내고
-//require("./calc.js") 경로를 찾아 화일을 연결하는 명령어
+console.log(color1, color2, color3);
